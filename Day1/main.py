255ca0a6bd377fad40f95d3fd65b9b86c0968ece
Damian <jadczakdamian@gmail.com>
2022-12-01 21:34:15 -0500

    Complete day1 solutions

--- Day1/main.py
+++ Day1/main.py
@@ -2,23 +2,33 @@
 with open('input.txt') as file:
     lines = file.readlines()
 
-def part1():
-    calories_list = []  # List the calories FOR EACH elf.
+# Parsing method to create a list of the combined calories for each elf
+def parse(list):
+    parsed_list = []  # List the total calories FOR EACH elf.
 
-    combined_cals = 0   # Initializing em
-    for element in lines:
+    combined_cals = 0   
+    # This loop will intentionally cause an error to stop
+    for i in range(len(list) + 1):
         try:
-            combined_cals += int(element)
+            combined_cals += int(list[i])
         except:     # Only a ValueError is expected
-            calories_list.append(combined_cals)     # haha logic in try/except
+            parsed_list.append(combined_cals)     # haha logic in try/except
             combined_cals = 0
+    return parsed_list
 
-    return max(calories_list)
+def part1():
+    return max(parse(lines))
 
 def part2():
+    calories_list = parse(lines)
     calorie_sum = 0
-    for i in range(3):
 
+    for i in range(3):
+        calorie_sum += max(calories_list)
+        calories_list.remove(max(calories_list))
+
+    return calorie_sum
 
 # MAIN
 print(f"Most calories contained by an elf: {part1()}")
+print(f"Total calories contained by the top 3 elves with the most snacks: {part2()}")
