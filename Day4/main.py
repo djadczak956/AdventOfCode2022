7b4ed5802d69c0d91d0026dc1a98271cd8cd9acf
Damian <jadczakdamian@gmail.com>
2022-12-08 20:15:26 -0500

    part2 funny solution

--- Day4/main.py
+++ Day4/main.py
@@ -21,6 +21,25 @@ def part1(input_list):
             sum += 1
 
     return sum
+
+def part2(input_list):
+    sum = 0
+    
+    for element in input_list:
+        first, second = element.split(',')
+        first_first = int(first.split('-')[0])
+        first_last = int(first.split('-')[-1])
+        second_first = int(second.split('-')[0])
+        second_last = int(second.split('-')[-1])
+
+        id1_list = [number for number in range(first_first, first_last + 1)]
+        id2_list = [number for number in range(second_first, second_last + 1)]
+
+        if any(num in id2_list for num in id1_list):
+            sum += 1
+
+    return sum
         
 
-print(part1(parse(lines)))
+# print(part1(parse(lines)))
+print(part2(parse(lines)))
