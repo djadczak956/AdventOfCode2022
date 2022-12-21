2d50a45d35d8f6d4338d7db2412a882f30df44c0
Damian Jadczak <jadczakdamian@gmail.com>
2022-12-21 13:36:21 -0500

    Stuff

--- Day5/main.py
+++ Day5/main.py
@@ -1,4 +1,5 @@
 from collections import deque
+import string
 
 with open('test-input.txt') as file:
     lines = file.readlines()
@@ -9,26 +10,37 @@ def parse(list):
         new_list.append(element.strip('\n'))
     return new_list
 
+# Read after the line break i.e. getting the directions
+def read_after_break(input_list):
+    directions = []
+    for element in input_list:
+        directions.append(element)
+        if element == "":   # Will cause the list to only 
+            directions = [] 
+    return directions
+
 def part1(input_list):
     letters = []
     # Adds lines until an empty line
-    print(input_list)
+    # Stops when past the crates
     for line in input_list:
         if "[" in line:
             letters.append(line)
         else:
             break
-        
-    
-    for col in range(5, len(letters), 4):
-        for row in letters[col]:
-            pass
 
-    return letters
+    stacks = []
+    for col in range(1, len(letters[0]), 4):
+        stacks.append([])
+        for row in range(len(letters)):
+            if letters[row][col].isalpha():
+                stacks[(col + 1) // 4].append(letters[row][col])
+
+    return stacks
     
 
 def part2():
     pass
 
 # --------------- MAIN ----------------------
-print(part1(parse(lines)))
+print(read_after_break(parse(lines)))
